5008c4b0704d166e4c6383274805eb902cdbf109
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-09-17 16:51:33 +0200

    Format lspClient.ts

--- src/lspClient.ts
+++ src/lspClient.ts
@@ -9,16 +9,16 @@ import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
-    TransportKind,
+	TransportKind,
 } from 'vscode-languageclient/node';
 import { westEnv } from './zephyr';
 import { existsSync, readFile } from 'fs';
 
-var client: LanguageClient
+var client: LanguageClient;
 
 export async function activate(ctx: vscode.ExtensionContext) {
-    vscode.commands.registerCommand('kconfig.add', () => {
-        vscode.window
+	vscode.commands.registerCommand('kconfig.add', () => {
+		vscode.window
 			.showOpenDialog({
 				canSelectFolders: true,
 				openLabel: 'Add',
@@ -29,117 +29,117 @@ export async function activate(ctx: vscode.ExtensionContext) {
 					addBuild(uris[0]);
 				}
 			});
-    });
+	});
 
-    const serverOptions: ServerOptions = {
-        command: 'python',
-        args: [path.resolve(ctx.extensionPath, 'srv', 'kconfiglsp.py'), '--log'],
-        options: {
-            cwd: vscode.workspace.workspaceFolders?.[0]?.uri.fsPath ?? process.cwd(),
-            env: westEnv,
-        },
-        transport: TransportKind.pipe,
-    };
+	const serverOptions: ServerOptions = {
+		command: 'python',
+		args: [path.resolve(ctx.extensionPath, 'srv', 'kconfiglsp.py'), '--log'],
+		options: {
+			cwd: vscode.workspace.workspaceFolders?.[0]?.uri.fsPath ?? process.cwd(),
+			env: westEnv,
+		},
+		transport: TransportKind.pipe,
+	};
 
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            {
-                pattern: '**/*.conf',
-            }
-        ],
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [
+			{
+				pattern: '**/*.conf',
+			},
+		],
 
-        diagnosticCollectionName: 'kconfig',
-    };
+		diagnosticCollectionName: 'kconfig',
+	};
 
-    client = new LanguageClient('Zephyr Kconfig', serverOptions, clientOptions);
-    client.start();
+	client = new LanguageClient('Zephyr Kconfig', serverOptions, clientOptions);
+	client.start();
 
-    const caches = await vscode.workspace.findFiles(
+	const caches = await vscode.workspace.findFiles(
 		'**/CMakeCache.txt',
 		'**/{twister,sanity}-out*'
 	);
 
-    await client.onReady();
+	await client.onReady();
 
-    caches.map((cache) =>
+	caches.map((cache) =>
 		addBuild(vscode.Uri.parse(path.dirname(cache.fsPath))).catch((err) => {
 			/* Ignore */
 		})
 	);
 
-    const cacheWatcher = vscode.workspace.createFileSystemWatcher('**/CMakeCache.txt');
+	const cacheWatcher = vscode.workspace.createFileSystemWatcher('**/CMakeCache.txt');
 
-    cacheWatcher.onDidChange(addBuild);
-    cacheWatcher.onDidCreate(addBuild);
-    cacheWatcher.onDidDelete(removeBuild);
+	cacheWatcher.onDidChange(addBuild);
+	cacheWatcher.onDidCreate(addBuild);
+	cacheWatcher.onDidDelete(removeBuild);
 }
 
 export function setMainBuild(uri?: vscode.Uri) {
-    client.sendNotification('kconfig/setMainBuild', {uri: uri?.toString() ?? ''});
+	client.sendNotification('kconfig/setMainBuild', { uri: uri?.toString() ?? '' });
 }
 
 interface AddBuildParams {
-    root: string;
-    env: typeof process.env;
-    conf: string[]
+	root: string;
+	env: typeof process.env;
+	conf: string[];
 }
 
 interface CMakeCache {
-    [name: string]: string[];
+	[name: string]: string[];
 }
 
 function parseCmakeCache(uri: vscode.Uri): Promise<CMakeCache> {
-    return new Promise<CMakeCache>((resolve, reject) => {
-        readFile(uri.fsPath, {encoding: 'utf-8'}, (err, data) =>{
-            if (err) {
-                reject(err);
-            } else {
-                const lines = data.split(/\r?\n/g);
-                const entries: CMakeCache = {};
-                lines.forEach(line => {
-                    const match = line.match(/^(\w+)(?::\w+)?\=(.*)/);
-                    if (match) {
-                        entries[match[1]] = match[2].trim().split(';');
-                    }
-                });
+	return new Promise<CMakeCache>((resolve, reject) => {
+		readFile(uri.fsPath, { encoding: 'utf-8' }, (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				const lines = data.split(/\r?\n/g);
+				const entries: CMakeCache = {};
+				lines.forEach((line) => {
+					const match = line.match(/^(\w+)(?::\w+)?\=(.*)/);
+					if (match) {
+						entries[match[1]] = match[2].trim().split(';');
+					}
+				});
 
-                resolve(entries);
-            }
-        })
-    })
+				resolve(entries);
+			}
+		});
+	});
 }
 
 interface ZephyrModule {
-    name: string;
-    path: string;
+	name: string;
+	path: string;
 }
 
 function parseZephyrModules(uri: vscode.Uri): Promise<ZephyrModule[]> {
-    return new Promise<ZephyrModule[]>((resolve, reject) => {
-        readFile(uri.fsPath, {encoding: 'utf-8'}, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                const lines = data.split(/\r?\n/g);
-                const modules = new Array<ZephyrModule>();
-                lines.forEach(line => {
-                    const match = line.match(/^"([^"]+)":"([^"]+)"/);
-                    if (match) {
-                        modules.push({
-                            name: match[1],
-                            path: match[2],
-                        });
-                    }
-                });
+	return new Promise<ZephyrModule[]>((resolve, reject) => {
+		readFile(uri.fsPath, { encoding: 'utf-8' }, (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				const lines = data.split(/\r?\n/g);
+				const modules = new Array<ZephyrModule>();
+				lines.forEach((line) => {
+					const match = line.match(/^"([^"]+)":"([^"]+)"/);
+					if (match) {
+						modules.push({
+							name: match[1],
+							path: match[2],
+						});
+					}
+				});
 
-                resolve(modules);
-            }
-        })
-    })
+				resolve(modules);
+			}
+		});
+	});
 }
 
 interface BuildResponse {
-    id: string;
+	id: string;
 }
 
 export async function addBuild(uri: vscode.Uri) {
@@ -150,7 +150,7 @@ export async function addBuild(uri: vscode.Uri) {
 	const boardDir = cache['BOARD_DIR'][0];
 	const arch = path.basename(path.dirname(boardDir));
 
-    const appDir = cache['APPLICATION_SOURCE_DIR'][0];
+	const appDir = cache['APPLICATION_SOURCE_DIR'][0];
 	const appKconfig = path.join(appDir, 'Kconfig');
 	const zephyrKconfig = path.join(cache['ZEPHYR_BASE'][0], 'Kconfig');
 
@@ -186,7 +186,7 @@ export async function addBuild(uri: vscode.Uri) {
 	};
 
 	modules.forEach((module) => {
-        const name = module.name.toUpperCase().replace(/[^\w]/g, '_');
+		const name = module.name.toUpperCase().replace(/[^\w]/g, '_');
 		env[`ZEPHYR_${name}_MODULE_DIR`] = module.path;
 		env[`ZEPHYR_${name}_KCONFIG`] = path.join(module.path, 'Kconfig');
 	});
@@ -196,81 +196,85 @@ export async function addBuild(uri: vscode.Uri) {
 		DTS_POST_CPP: vscode.Uri.joinPath(uri, 'zephyr', `${board}.dts.pre.tmp`).fsPath,
 		DTS_ROOT_BINDINGS: cache['CACHED_DTS_ROOT_BINDINGS'].join('?'),
 
-        // KCONFIG_FUNCTIONS: path.join(cache['ZEPHYR_BASE'][0], 'scripts', 'kconfig', 'kconfigfunctions')
+		// KCONFIG_FUNCTIONS: path.join(cache['ZEPHYR_BASE'][0], 'scripts', 'kconfig', 'kconfigfunctions')
 	});
 
 	return client.sendRequest<BuildResponse>('kconfig/addBuild', {
-        uri: uri.toString(),
+		uri: uri.toString(),
 		root,
 		env,
-		conf: cache['CACHED_CONF_FILE']?.map(file => path.resolve(appDir, file)) ?? [],
+		conf: cache['CACHED_CONF_FILE']?.map((file) => path.resolve(appDir, file)) ?? [],
 	} as AddBuildParams);
 }
 
 export async function removeBuild(uri: vscode.Uri) {
-    client.sendNotification('kconfig/removeBuild', { uri: uri.toString() });
+	client.sendNotification('kconfig/removeBuild', { uri: uri.toString() });
 }
 
 interface GenericNode {
-    kind: 'symbol' | 'choice' | 'comment' | 'menu' | 'unknown';
-    visible: boolean;
-    loc?: vscode.Location;
-    isMenu: boolean;
-    hasChildren: boolean;
-    depth: number;
-    id: string;
-    prompt?: string;
-    help?: string;
+	kind: 'symbol' | 'choice' | 'comment' | 'menu' | 'unknown';
+	visible: boolean;
+	loc?: vscode.Location;
+	isMenu: boolean;
+	hasChildren: boolean;
+	depth: number;
+	id: string;
+	prompt?: string;
+	help?: string;
 }
 
 interface SymbolNode extends GenericNode {
-    kind: 'symbol';
-    type: 'unknown' | 'bool' | 'tristate' | 'string' | 'int' | 'hex';
-    val: string;
-    name: string;
-    options: string[];
-    userValue: string;
+	kind: 'symbol';
+	type: 'unknown' | 'bool' | 'tristate' | 'string' | 'int' | 'hex';
+	val: string;
+	name: string;
+	options: string[];
+	userValue: string;
 }
 
 interface CommentNode extends GenericNode {
-    kind: 'comment';
+	kind: 'comment';
 }
 
 interface ChoiceNode extends GenericNode {
-    kind: 'choice';
-    val: string | undefined;
+	kind: 'choice';
+	val: string | undefined;
 }
 
 interface MenuNode extends GenericNode {
-    kind: 'menu';
+	kind: 'menu';
 }
 
 interface UnknownNode extends GenericNode {
-    kind: 'unknown';
+	kind: 'unknown';
 }
 
 export type Node = SymbolNode | CommentNode | ChoiceNode | MenuNode | UnknownNode;
 
 export interface Menu {
-    name: string;
-    id: string;
-    items: Node[];
+	name: string;
+	id: string;
+	items: Node[];
 }
 
 export interface MenuOptions {
-    allSymbols?: boolean;
+	allSymbols?: boolean;
 }
 
-export async function getMenu(uri?: vscode.Uri, node?: string, options: MenuOptions={}): Promise<Menu> {
-    const rsp = await client.sendRequest<any>('kconfig/getMenu', {
+export async function getMenu(
+	uri?: vscode.Uri,
+	node?: string,
+	options: MenuOptions = {}
+): Promise<Menu> {
+	const rsp = await client.sendRequest<any>('kconfig/getMenu', {
 		ctx: uri?.toString(),
 		id: node,
 		options,
 	});
-    return <Menu> {
-        ...rsp as Menu,
-        items: rsp.items.map((item: any) => {
-            return {
+	return <Menu>{
+		...(rsp as Menu),
+		items: rsp.items.map((item: any) => {
+			return {
 				...(item as GenericNode),
 				// Convert URI from string to URI object:
 				loc: new vscode.Location(
@@ -283,7 +287,6 @@ export async function getMenu(uri?: vscode.Uri, node?: string, options: MenuOpti
 					)
 				),
 			};
-        })
-
-    }
+		}),
+	};
 }
